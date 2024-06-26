7b791d89eb985c4a899d14147e41446ddb0b85f8
Ucmessina <ucmessina@alumnostrazos.net>
2024-06-26 14:02:32 +0200

    +FirstCommit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const formElement = document.getElementById('form');
+
+const checkForm = event => {
+  event.preventDefault();
+};
+
+const checkinput = event => {
+  console.log(event.target.value);
+};
+
+formElement.addEventListener('submit', checkForm);
+formElement.addEventListener('input', checkinput);
